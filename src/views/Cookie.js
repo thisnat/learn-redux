26d2685bdc05efbb58b39e7a087ca5f03e05f980
Nattawut <thisnattawut@gmail.com>
2021-06-03 22:17:54 +0700

    learn cookie

--- src/views/Cookie.js
+++ src/views/Cookie.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import { useState } from 'react'
 
 const Cookie = () => {
+    const [cookie,setCookie] = useState('');
+
+    const handleEditBtn = (e) => {
+        e.preventDefault();
+
+        document.cookie = cookie;
+        setCookie('');
+    }
+
     return (
         <div>
-            <h1>hello</h1>
+            <h1 style={{fontSize:64}}>cookie ,localstorage and session things 🥴</h1>
+            <div className="container" style={{marginTop : "4rem"}}>
+                <p className="display-4">current cookie</p>
+                <p className="lead">🍪 : {document.cookie}</p>
+                <div className="mt-3">
+                    <div className="form-group">
+                        <p className="display-4">edit current cookie</p>
+                        <input type="text" className="form-control" onChange={(e) => {
+                            setCookie(e.target.value);
+                        }} />
+                        <button className="btn btn-success mt-3" onClick={handleEditBtn}>edit</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
